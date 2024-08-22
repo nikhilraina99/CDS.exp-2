68c5f0f96db9f696f16664db0714972a3b275c53
nikhilraina99 <rainanikhilr@gmail.com>
2024-08-23 01:00:08 +0530

    Update exp2a.cpp

--- exp2a.cpp
+++ exp2a.cpp
@@ -5,7 +5,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char a[100];
+    char a[120];
     cout<<"size of int "<<sizeof(int)<<" char "<<sizeof(char)<<" float "<<sizeof(float)
     <<" double "<<sizeof(string)<<" long "<<sizeof(bool)<<" array "<<sizeof(a);
       return 0; }
